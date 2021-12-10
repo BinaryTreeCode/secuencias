03f4e9e7424a6a801bf7bf17502dfe5cc2b98275
Adrián Torres <binarytreecode@gmail.com>
2021-12-10 11:28:27 -0500

    hice la estructura html

--- secuencia.js
+++ secuencia.js
@@ -1,16 +1,40 @@
-var razon_id = document.getElementById("razon");
-var largo_id = document.getElementById("largo");
-var valor_inicial_id = document.getElementById("value_inicial");
 var type_sequence_id = document.getElementById("type_razón");
-var type_problem_id = document.getElementById("type_problem");
+var razon_id = document.getElementById("razon");
+
+var pre_valor_inicial = document.getElementById("value_inicial");
+var valor_inicial_id2 = document.getElementById("value_inicial_problem");
+
+var pre_largo = document.getElementById("largo");
+var largo_id2 = document.getElementById("largo_problem");
+
+var pre_type_problem = document.getElementById("type_problem");
+var ype_problem_id2 = document.getElementById("type_problem_problem");
+
+var Box = document.getElementById("check");
+
+var pre_valor_inicial = pre_valor_inicial
+var pre_largo = pre_largo;
+var pre_type_problem = pre_type_problem;
+
+Box.addEventListener ("change", function () {
+    checkBox = Box.checked;
+    if (checkBox == true) {
+        pre_valor_inicial = valor_inicial_id2;
+        pre_largo = largo_id2;
+        pre_type_problem = ype_problem_id2;
+    }
+    else {
+    }
+});
+
 
 let type_sequence, type_problem, problema;
 function modulo() {
-    inicio = parseInt(valor_inicial_id.value);
+    inicio = parseInt(pre_valor_inicial.value);
     razon = parseFloat(razon_id.value);
-    largo = parseInt(largo_id.value);
+    largo = parseInt(pre_largo.value);
     type_sequence = type_sequence_id.value;
-    type_problem = type_problem_id.value;
+    type_problem = pre_type_problem.value;
 
     secuencia.push(inicio);
     tiempo.push(1);
@@ -198,7 +222,25 @@ function imprimir() {
     }
     resultado.innerHTML = txt;
 }
+var resultado;
+var btn_posición = document.getElementById("btn_posición");
 
+btn_posición.addEventListener("click", function () { 
+    resultado = document.getElementById("resultado_razon_posición");
+    imprimir_razon()
+});
+
+var btn_array = document.getElementById("btn_array");
+btn_array.addEventListener("click", function () {
+    resultado = document.getElementById("resultado_razon_array");
+    imprimir_razon()
+});
+
+
+function imprimir_razon() {
+    var txt = "razon =" + razon;
+    resultado.innerHTML = txt;
+}
 
 var tip = false, diferencia, pre_razon;
 function hallar_razon(ti, tf, Pi, Pf) {
